87c6861343abf16b17c38580531dda06145e1726
ciue <aoue.hao@gmail.com>
2018-02-10 18:26:52 +0800

    #

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
+import member from './components/member.vue'
+import address from './components/address.vue'
+import all from './components/all.vue'
+import form from './components/form.vue'
+
 let routes = [{
     path: '/',
-    components: require('./components/member.vue')
-
+    component: member
+}, {
+    path: '/address',
+    component: address,
+    children: [{
+        path: '',
+        component: all,
+    }, {
+        path: 'all',
+        component: all,
+    }, {
+        path: 'form',
+        component: form
+    }]
 }]
+
 // route 实例
 let router = new Router({
     routes
@@ -14,6 +32,6 @@ let router = new Router({
 
 // 找到挂载点、根组件的注入
 new Vue({
-  el: '#app',
-  router
+    el: '#app',
+    router
 })
